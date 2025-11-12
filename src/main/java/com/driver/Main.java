45c9f1bbf299d29d7c02da30cfa809cac9c3539a
Nipu Kumari <nipu.c7@gmail.com>
2025-11-12 17:26:02 +0530

    method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,33 +1,9 @@
 package com.driver;
 
-class A {
-
-    String meth()
-    {
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-
-    String meth()
-    {
-        return "Method is overridden in Extended class B";
-    }
-}
-
 public class Main {
 
-<<<<<<< HEAD
     public static void main(String[] args) {
         B obj = new B();
         System.out.println(obj.meth());
     }
-=======
-    public static void main(String[] args)
-    {
-         B obj = new B();
-        System.out.println(obj.meth());
-    }
-  
->>>>>>> a0e6b808632047a869cf1ea6e26ba5d2bbf54f90
 }
